10fcf534f2ee7ef6b11a78cf1351696ba83d0052
Khiem Nguyen <khiemnd9112@gmail.com>
2023-09-07 11:14:43 +0700

    Finalizing portfolio page

--- src/main/resources/static/scripts/portfolio-scripts.js
+++ src/main/resources/static/scripts/portfolio-scripts.js
@@ -10,3 +10,14 @@ function scrollToFunction(destination) {
     let ele = document.getElementById(destination);
     ele.scrollIntoView({behavior: 'smooth', block: 'center'});
 }
+
+function enlargeImage(imageTN) {
+
+    let modal = document.getElementById('image-modal');
+    addClosingAction(modal);
+
+    let imageOfModal = modal.getElementsByClassName('image')[0];
+    imageOfModal.src = imageTN.src;
+
+    modal.style.display = 'block';
+}
